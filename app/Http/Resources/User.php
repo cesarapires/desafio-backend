b0ce3b387697de237ed0cb095cc984354a862505
cesarapires <cesar.augusto.pires@hotmail.com>
2021-10-29 00:04:57 -0300

    push on new branch, docker conflict resolution

---
+++ app/Http/Resources/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class User extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'idUser'=>$this->idUser,
+            'name'=>$this->name,
+            'birthday'=>$this->birthday,
+            'cpf'=>$this->cpf,
+            'email'=>$this->email
+        ];
+    }
+}
